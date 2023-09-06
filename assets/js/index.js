2c24019a40b6f96e2e0cb0355e2b12b906b4e3de
Parkseolmin <qkrtjfals2@naver.com>
2023-09-06 18:08:15 +0900

    😀 애니메이션 추가

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@
 const lenis = new Lenis();
 
 lenis.on('scroll', (e) => {
-  // console.log(e);
+  console.log(e);
 });
 
 function raf(time) {
@@ -51,6 +51,7 @@ $(function () {
 
 // .top-visual 이미지 슬라이드
 $(document).ready(function () {
+  // 첫 번째 슬라이드
   $('.visual .slide').slick({
     arrow: true, //화살표
     dots: true, //인디케이터
@@ -61,4 +62,39 @@ $(document).ready(function () {
     pauseOnFocus: false,
   });
   $('.slick-prev').text('prev');
+
+  // 두 번째 슬라이드
+  $('.slide2').slick({
+    arrows: false, //화살표
+    dots: true, //인디케이터
+    autoplay: true, //자동재생
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplaySpeed: 6000, //재생시간
+    pauseOnHover: true, //호버시 멈추는 멈추도록 설정함
+    pauseOnFocus: true,
+  });
+  $('.slide2 #slick-slide-control10').text('서울 마곡지구 업무용지');
+  $('.slide2 #slick-slide-control11').text('서울 마곡지구 대방디엠시티2차');
+  $('.slide2 #slick-slide-control12').text(
+    '화성 동탄 1차 대방디엠시티 더 센텀'
+  );
+  $('.slide2 #slick-slide-control13').text('광주 수완 대방노블랜드6차');
+});
+
+// 스크롤라 애니메이션(scrolla.js)
+$(function () {
+  $('.animate').scrolla({
+    mobile: true, // 모바일버전시 활성화
+    once: false, //스크롤이 딱 한번만 하고 싶을 때는 true
+  });
+});
+
+// 스크롤다운
+$(function () {
+  $('.scroll').on('click', function () {
+    let scrollBtn = $('#scroll').offset().top; //$scroll이 끝나는 지점으로 스크롤바 이동
+    $('html, body').animate({ scrollTop: scrollBtn }, 400); //그 위치로 부드럽게 스크롤이동
+  });
 });
